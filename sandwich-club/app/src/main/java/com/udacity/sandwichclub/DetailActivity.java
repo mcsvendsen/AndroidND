a32215045b4d330b412c123492029c24a58304b7
Myles <myles.svendsen+git@gmail.com>
2018-04-26 07:58:20 -0400

    Added comments, polished code

--- sandwich-club/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ sandwich-club/app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -16,9 +16,11 @@ import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
 
+    /* constants for our Intent positions */
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    /* we will use Activity Binding to load sandwich information to the UI */
     ActivityDetailBinding dBinding;
 
     @Override
@@ -26,14 +28,19 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        /* get a reference to our Image View so that we can bind sandwich images to it */
         ImageView ingredientsIv = (ImageView) findViewById(R.id.image_iv);
+
+        /* initialize our Activity Binding object by pointing to the Activity Detail layout */
         dBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
 
+        /* get the user's intention and close app if it doesn't exist */
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
         }
 
+        /* find the sandwich selected by the user and close app if not found */
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
@@ -41,8 +48,11 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
+        /* create the JSON */
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
+
+        /* parse the JSON into a Sandwich object and close the app if Sandwich not created */
         Sandwich sandwich = null;
         try { sandwich = JsonUtils.parseSandwichJson(json); }
         catch (JSONException e) {
@@ -55,27 +65,38 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
+        /* display all sandwich details on the screen
+           * CREDITS:
+           * loading.gif drawable borrowed from:
+           * https://dribbble.com/shots/470914-Animated-8-Bit-Hourglass
+           * error.jpg drawable borrowed from:
+           * http://blog.jaringanhosting.com/wp-content/uploads/2016/01/Fix-503-Service-Unavailable-Error-in-ASP.NET_.jpeg */
         populateUI(sandwich);
-        //TEST CODE - WILL BE DELETED ingredientsIv.setImageResource(R.mipmap.ic_launcher);
-        //TEST CODE - WILL BE DELETED     .placeholder(R.drawable.loading)
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.loading)
+                .error(R.drawable.error)
                 .into(dBinding.imageIv);
 
         setTitle(sandwich.getMainName());
     }
 
+    /* This method passes a detailed error message to the user via Toast */
     private void closeOnError() {
         finish();
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
+    /* This method parses a Sandwich object, binding the sandwich details to the screen */
     private void populateUI(Sandwich sandwich) {
 
+        /* Sandwich description */
         dBinding.descriptionTv.setText(sandwich.getDescription());
 
+        /* Sandwich also-known-as name(s)
+         * NOTE: if none given, will default to "N/A" */
         StringBuilder akaText = new StringBuilder();
-        if (sandwich.getAlsoKnownAs().size() > 0) {
+        if (!sandwich.getAlsoKnownAs().isEmpty()) {
             for (int i = 0; i < sandwich.getAlsoKnownAs().size(); i++) {
                 akaText.append(sandwich.getAlsoKnownAs().get(i));
                 if (i < (sandwich.getAlsoKnownAs().size() - 1)) akaText.append(", ");
@@ -83,16 +104,20 @@ public class DetailActivity extends AppCompatActivity {
         }else akaText.append("N/A");
         dBinding.alsoKnownTv.setText(akaText.toString());
 
+        /* Sandwich ingredients list
+         * NOTE: if not specified, will default to "Unknown" */
         StringBuilder ingredientsText = new StringBuilder();
         if (sandwich.getIngredients().size() > 0) {
             for (int i = 0; i < sandwich.getIngredients().size(); i++) {
                 ingredientsText.append(sandwich.getIngredients().get(i));
                 if (i != (sandwich.getIngredients().size() - 1)) ingredientsText.append(", ");
             }
-        }else ingredientsText.append("Unknown");
+        }else ingredientsText.append(R.string.unknown_info_message);
         dBinding.ingredientsTv.setText(ingredientsText.toString());
 
-        if (sandwich.getPlaceOfOrigin().isEmpty()) dBinding.originTv.setText("Unknown");
+        /* Sandwich place of origin
+         * NOTE: if not specified, will default to "Unknown" */
+        if (sandwich.getPlaceOfOrigin().isEmpty()) dBinding.originTv.setText(R.string.unknown_info_message);
         else dBinding.originTv.setText(sandwich.getPlaceOfOrigin());
     }
 }
--- sandwich-club/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,5 +1,6 @@
 package com.udacity.sandwichclub.utils;
 
+import com.udacity.sandwichclub.R;
 import com.udacity.sandwichclub.model.Sandwich;
 
 import org.json.JSONArray;
@@ -11,6 +12,8 @@ import java.util.List;
 
 public class JsonUtils {
 
+    /* Constants representing the JSON structure
+     * NOTE: if the JSON structure is changed, these constants will need to be updated */
     private static final String SCM_NAMELIST = "name";
     private static final String SCM_MAINNAME = "mainName";
     private static final String SCM_AKA = "alsoKnownAs";
@@ -19,35 +22,42 @@ public class JsonUtils {
     private static final String SCM_IMAGE = "image";
     private static final String SCM_INGREDIENTSLIST = "ingredients";
 
+    /* This method will parse a JSON string into a returnable Sandwich object */
     public static Sandwich parseSandwichJson(String json) throws JSONException{
 
         Sandwich returnSandwich = new Sandwich();
 
+        /* Create a JSON object from the passed-in string */
         JSONObject sandwichJson = null;
-
         try { sandwichJson = new JSONObject(json); }
         catch (Exception e) {
             e.printStackTrace();
             return null;
         }
 
-        //JSONObject mainNameObject = sandwichJson.getJSONArray(SCM_NAMELIST).getString(0);
-        //returnSandwich.setMainName(mainNameObject.getString(SCM_MAINNAME));
+        /* Set the Sandwich main name */
         returnSandwich.setMainName(sandwichJson.getJSONObject(SCM_NAMELIST).getString(SCM_MAINNAME));
 
+        /* Set the Sandwich also-known-as name(s) */
         JSONArray akaArray = sandwichJson.getJSONObject(SCM_NAMELIST).getJSONArray(SCM_AKA);
         List<String> akaNames = new ArrayList<String>();
-        for (int i = 0; i < akaArray.length(); i++) {
-            akaNames.add(akaArray.getString(i));
+        if (akaArray.length() > 0) {
+            for (int i = 0; i < akaArray.length(); i++) {
+                akaNames.add(akaArray.getString(i));
+            }
         }
         returnSandwich.setAlsoKnownAs(akaNames);
 
+        /* Set the Sandwich place of origin */
         returnSandwich.setPlaceOfOrigin(sandwichJson.getString(SCM_PLACEOFORIGIN));
 
+        /* Set the Sandwich description */
         returnSandwich.setDescription(sandwichJson.getString(SCM_DESC));
 
+        /* Set the Sandwich image */
         returnSandwich.setImage(sandwichJson.getString(SCM_IMAGE));
 
+        /* Set the Sandwich ingredients */
         JSONArray ingredientsArray = sandwichJson.getJSONArray(SCM_INGREDIENTSLIST);
         List<String> ingredients = new ArrayList<String>();
         for (int i = 0; i < ingredientsArray.length(); i++) {
